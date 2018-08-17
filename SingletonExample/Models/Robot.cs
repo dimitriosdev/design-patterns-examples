acce59f9a94102a63f9c530c77ffc27ce3a9ad59
Dimitrios Metozis <dimitrios.dev@gmail.com>
2018-08-17 12:16:11 +0200

    Initial commit.

---
+++ SingletonExample/Models/Robot.cs
@@ -0,0 +1,22 @@
+﻿namespace SingletonExample.Models
+{
+    public class Robot
+    {
+        private static Robot self;
+        private string name;
+        private Robot() {}
+
+        public static Robot Instance()
+        {
+            if (self == null)
+                self = new Robot();
+            return self;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
---
+++ SingletonExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using SingletonExample.Models;
+using System;
+
+namespace SingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot a = Robot.Instance();
+            Robot b = Robot.Instance();
+            a.Name = "Dimi";
+
+            
+            Console.WriteLine("Robot's a name is: " + a.Name);
+            Console.WriteLine("Robot's b name is: " + b.Name);
+            Console.WriteLine("Oups. It is the same robot.");
+            Console.ReadKey();
+        }
+    }
+}
